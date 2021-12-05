30fc76d886dd8e3d641a41fac177d38af62d5874
rishcodelib <ri.sbh23@gmail.com>
2021-12-05 19:46:44 +0530

    Restful-API-One

---
+++ lab1-RESTful-API/src/main/java/demo/springboot/service/UserService.java
@@ -0,0 +1,26 @@
+package demo.springboot.service;
+
+
+import java.util.List;
+
+import demo.springboot.model.User;
+
+public interface UserService {
+	
+	User findById(long id);
+	
+	User findByName(String name);
+	
+	void saveUser(User user);
+	
+	void updateUser(User user);
+	
+	void deleteUserById(long id);
+
+	List<User> findAllUsers();
+	
+	void deleteAllUsers();
+	
+	boolean isUserExist(User user);
+	
+}
