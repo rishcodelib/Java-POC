fcb1bf8f8b006ef35fd96ad79151f9b5184fca1d
Rishabh-Lumen <Rishabh.Aggarwal@centurylink.com>
2022-01-12 22:55:55 +0530

    updating

---
+++ ex2-Spring-Security-jwt/src/main/java/com/aggarwal/ex2/Ex2SpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.aggarwal.ex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex2SpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex2SpringSecurityJwtApplication.class, args);
+	}
+
+}
