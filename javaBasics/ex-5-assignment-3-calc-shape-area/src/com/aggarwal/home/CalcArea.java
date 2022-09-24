16c3cfa8df51d9f4861dab265c6dd9af1dcc4d86
rishcodelib <ri.sbh23@gmail.com>
2022-09-24 22:42:18 +0530

    calculate shape area assignment

---
+++ javaBasics/ex-5-assignment-3-calc-shape-area/src/com/aggarwal/home/CalcArea.java
@@ -0,0 +1,12 @@
+package com.aggarwal.home;
+
+public class CalcArea {
+
+	// Display the area of a shape given it's required dimension
+
+	public static void main(String[] args) {
+
+		System.out.println("Enter t");
+	}
+
+}
