5432548af83e72dd7d12ab03c604b0d65894a0e3
rishcodelib <ri.sbh23@gmail.com>
2022-09-24 22:30:53 +0530

    prime number program

--- javaBasics/ex-4-assignment-2-prime-number/src/com/assignment/PrimeNumber.java
+++ javaBasics/ex-4-assignment-2-prime-number/src/com/assignment/PrimeNumber.java
@@ -1,10 +1,39 @@
 package com.assignment;
 
+import java.util.Scanner;
+
 public class PrimeNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int i, m = 0, flag = 0;
+
+		System.out.println("Enter the  number to check prime.");
+
+		Scanner sc = new Scanner(System.in);
+
+		// Getting Number
+
+		int n = sc.nextInt();
+
+		// Dividing the number by 2
+
+		m = n / 2;
+
+		for (i = 2; i <= m; i++) {
+
+			if (n % i == 0) {
+				System.out.println("Not a Prime");
+				flag = 1;
+				break;
+			}
+
+		}
+
+		if (flag == 0) {
+			System.out.println("Number is Prime");
+		}
+		
 	}
 
 }
